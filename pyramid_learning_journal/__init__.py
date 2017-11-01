5e2b7a62bdb62c76aa500112c7eeab7507e2d7a5
Kevin Robinson <kevinr.zan@gmail.com>
2017-11-01 16:33:16 -0700

    step1 progress

--- pyramid_learning_journal/__init__.py
+++ pyramid_learning_journal/__init__.py
@@ -1,3 +1,4 @@
+'''Main init file'''
 from pyramid.config import Configurator
 
 
@@ -8,5 +9,6 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.include('.models')
     config.include('.routes')
+    config.include('.views')
     config.scan()
     return config.make_wsgi_app()
