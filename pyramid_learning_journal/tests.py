413f953b7443123d45f63181df6f95d29213e423
Allan Liebold <allan.liebold@gmail.com>
2017-11-01 18:03:07 -0700

    moved bootstrap files into static directory and changed references in list.html to match.

--- pyramid_learning_journal/tests.py
+++ pyramid_learning_journal/tests.py
@@ -1,65 +1,29 @@
-import unittest
-import transaction
+from pyramid.response import Response
+import os
+import io
 
-from pyramid import testing
-
-
-def dummy_request(dbsession):
-    return testing.DummyRequest(dbsession=dbsession)
+HERE = os.path.dirname(__file__)
 
 
-class BaseTest(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp(settings={
-            'sqlalchemy.url': 'sqlite:///:memory:'
-        })
-        self.config.include('.models')
-        settings = self.config.get_settings()
-
-        from .models import (
-            get_engine,
-            get_session_factory,
-            get_tm_session,
-            )
-
-        self.engine = get_engine(settings)
-        session_factory = get_session_factory(self.engine)
-
-        self.session = get_tm_session(session_factory, transaction.manager)
-
-    def init_database(self):
-        from .models.meta import Base
-        Base.metadata.create_all(self.engine)
-
-    def tearDown(self):
-        from .models.meta import Base
-
-        testing.tearDown()
-        transaction.abort()
-        Base.metadata.drop_all(self.engine)
+def list_view(request):
+    path = os.path.join(HERE, '../templates/index.html')
+    with io.open(path) as file:
+        return Response(file.read())
 
 
-class TestMyViewSuccessCondition(BaseTest):
-
-    def setUp(self):
-        super(TestMyViewSuccessCondition, self).setUp()
-        self.init_database()
-
-        from .models import MyModel
-
-        model = MyModel(name='one', value=55)
-        self.session.add(model)
-
-    def test_passing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'Pyramid Learning Journal')
+def detail_view(request):
+    path = os.path.join(HERE, '../templates/detail.html')
+    with io.open(path) as file:
+        return Response(file.read())
 
 
-class TestMyViewFailureCondition(BaseTest):
+def create_view(request):
+    path = os.path.join(HERE, '../templates/create.html')
+    with io.open(path) as file:
+        return Response(file.read())
 
-    def test_failing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info.status_int, 500)
+
+def update_view(request):
+    path = os.path.join(HERE, '../templates/update.html')
+    with io.open(path) as file:
+        return Response(file.read())
