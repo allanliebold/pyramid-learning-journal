b030265a3e6560f8cf28ed6442279feaa312dafb
Allan Liebold <allan.liebold@gmail.com>
2017-11-02 17:57:12 -0700

    created jinja2 template for detail view and modified views default.py

--- pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/views/default.py
@@ -1,29 +1,39 @@
-from pyramid.response import Response
-import os
-import io
-
-HERE = os.path.dirname(__file__)
+"""Views default.py file."""
+from pyramid.view import view_config
 
 
+@view_config(route_name='list', renderer='../templates/list.jinja2')
 def list_view(request):
-    path = os.path.join(HERE, '../templates/list.html')
-    with io.open(path) as file:
-        return Response(file.read())
+    """List view."""
+    return 'List View'
 
 
+@view_config(route_name='detail', renderer='../templates/detail.jinja2')
 def detail_view(request):
-    path = os.path.join(HERE, '../templates/detail.html')
-    with io.open(path) as file:
-        return Response(file.read())
+    """Detail view."""
+    return {
+        "category": "Day 11",
+        "creation_date": "October 30, 2017",
+        "content": ("We got our introduction to the Pyramid framework today."
+                    "It doesn't seem to be too complicated... but I don't want"
+                    " to make any assumptions about how simple this will be "
+                    "to learn. At first glance it seems like you're given the basic"
+                    " structure and you just make changes and add in what you "
+                    "want... like some sort of framework. Yeah. I followed along "
+                    "decently with the example during lecture, but I need to go "
+                    "over it again. Other than that we learned about the "
+                    "Double-Ended Queue, or Deque. Nothing too crazy or very "
+                    "different from what we've done so far with data structures.")
+    }
 
 
+@view_config(route_name='create', renderer='../templates/create.jinja2')
 def create_view(request):
-    path = os.path.join(HERE, '../templates/create.html')
-    with io.open(path) as file:
-        return Response(file.read())
+    """Create view."""
+    return 'Create View'
 
 
+@view_config(route_name='update', renderer='../templates/update.jinja2')
 def update_view(request):
-    path = os.path.join(HERE, '../templates/update.html')
-    with io.open(path) as file:
-        return Response(file.read())
+    """Update view."""
+    return 'Update View'
