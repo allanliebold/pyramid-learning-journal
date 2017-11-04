0a976c93141f27ff40d4ea782d31d9f31f053a12
Allan Liebold <allan.liebold@gmail.com>
2017-11-04 12:17:51 -0700

    layout page loads correctly and links to create page.

--- pyramid_learning_journal/testing.py
+++ pyramid_learning_journal/testing.py
@@ -1,26 +1,55 @@
+"""Testing for Learning Journal."""
+
 from pyramid import testing
-from pyramid.response import Response
+from pyramid_learning_journal.data.entry_data import ENTRIES
+import pytest
 
 
-def test_list_view_returns_response():
-    """List view returns a Response object."""
+@pytest.fixture()
+def testapp():
+    """Create an instance for testing."""
+    from pyramid_learning_journal import main
+    app = main({})
+    from webtest import TestApp
+    return TestApp(app)
+
+
+def test_root_contents(testapp):
+    """Test contents of root page contents."""
+    from pyramid_learning_journal.data.entry_data import ENTRIES
+    response = testapp.get('/', status=200)
+    html = response.html
+    assert len(ENTRIES) == len(html.findAll("article"))
+
+
+def test_list_view_returns_dict():
+    """List view returns response."""
     from pyramid_learning_journal.views.default import list_view
     request = testing.DummyRequest()
     response = list_view(request)
-    assert isinstance(response, Response)
+    assert isinstance(response, dict)
 
 
-def test_list_view_ok():
-    """List view response has a status 200 OK."""
+def test_list_view_returns_len_content():
+    """List view response has correct amount of content."""
     from pyramid_learning_journal.views.default import list_view
     request = testing.DummyRequest()
     response = list_view(request)
-    assert response.status_code == 200
+    assert len(response['entries']) == len(ENTRIES)
 
 
-def test_create_view_ok():
-    """Create view response has a status 200 OK."""
-    from pyramid_learning_journal.views.default import create_view
+def test_detail_view():
+    """Test detail view returns dictionary of values."""
+    from pyramid_learning_journal.views.default import detail_view
     request = testing.DummyRequest()
-    response = create_view(request)
-    assert response.status_code == 200
+    info = detail_view(request)
+    assert isinstance(info, dict)
+
+
+def test_detail_view_response_contains_expense_attrs():
+    """Test detail view returns entry."""
+    from pyramid_learning_journal.views.default import detail_view
+    request = testing.DummyRequest()
+    info = detail_view(request)
+    for key in ["created", "title", "text"]:
+        assert key in info.keys()
--- pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/views/default.py
@@ -1,5 +1,6 @@
 """Views default.py file."""
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPNotFound
 from pyramid_learning_journal.data.entry_data import ENTRIES
 
 
@@ -12,16 +13,28 @@ def list_view(request):
 @view_config(route_name='detail', renderer='../templates/detail.jinja2')
 def detail_view(request):
     """Detail view."""
-    return {}
+    target = int(request.matchdict('id'))
+    for entry in ENTRIES:
+        if entry['id'] == target:
+            return {
+                'entry': entry
+            }
+    raise HTTPNotFound
 
 
 @view_config(route_name='create', renderer='../templates/create.jinja2')
 def create_view(request):
     """Create view."""
-    return 'Create View'
+    return {}
 
 
 @view_config(route_name='update', renderer='../templates/update.jinja2')
 def update_view(request):
     """Update view."""
-    return 'Update View'
+    target = int(request.matchdict('id'))
+    for entry in ENTRIES:
+        if entry['id'] == target:
+            return {
+                'entry': entry
+            }
+    raise HTTPNotFound
