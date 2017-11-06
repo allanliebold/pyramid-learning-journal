10696df7a49e658ef0f3bf4327421a01d441fd94
Allan Liebold <allan.liebold@gmail.com>
2017-11-05 20:33:26 -0800

    adds test fixtures.

--- pyramid_learning_journal/testing.py
+++ pyramid_learning_journal/testing.py
@@ -1,6 +1,8 @@
 """Testing for Learning Journal."""
 
 import pytest
+from datetime import datetime
+from faker import Faker
 from pyramid import testing
 import transaction
 from pyramid_learning_journal.models import (
@@ -10,13 +12,45 @@ from pyramid_learning_journal.models import (
 from pyramid_learning_journal.models.meta import Base
 
 
-@pytest.fixture
-def testapp():
+FAKE_FACTORY = Faker()
+ENTRY_LIST = [Entry(
+    title="Test Entry",
+    body=FAKE_FACTORY.text(100),
+    created=datetime.now(),
+) for i in range(20)]
+
+
+@pytest.fixture(scope="session")
+def testapp(request):
     """Create an instance for testing."""
-    from pyramid_learning_journal import main
-    app = main({})
     from webtest import TestApp
-    return TestApp(app)
+    from pyramid_learning_journal import main
+
+    app = main({}, **{"sqlalchemy.url": "postgres:///test_entries"})
+    SessionFactory = app.registry["dbsession_factory"]
+    engine = SessionFactory().bind
+    Base.metadata.create_all(bind=engine)
+
+    request.addfinalizer(tearDown)
+
+    return testapp
+
+
+@pytest.fixture
+def fill_the_db(testapp):
+    """Add items to the database."""
+    SessionFactory = testapp.app.registry["dbsession_factory"]
+    with transaction.manager:
+        dbsession = get_tm_session(SessionFactory, transaction.manager)
+        dbsession.add_all(ENTRY_LIST)
+
+    return dbsession
+
+
+@pytest.fixture
+def add_models(dummy_request):
+    """Add model instances to the database."""
+    dummy_request.dbsession.add_all(ENTRY_LIST)
 
 
 @pytest.fixture(scope="session")
@@ -56,6 +90,18 @@ def dummy_request(db_session):
     return testing.DummyRequest(dbsession=db_session)
 
 
+def test_model_gets_added(db_session):
+    """Test adding model to database."""
+    assert len(db_session.query(Entry).all()) == 0
+    model = Entry(
+        title="Test Entry",
+        body="This is a test entry.",
+        created=datetime.now(),
+    )
+    db_session.add(model)
+    assert len(db_session.query(Entry).all()) == 1
+
+
 def test_list_view_returns_dict(dummy_request):
     """List view returns response."""
     from pyramid_learning_journal.views.default import list_view
@@ -63,11 +109,19 @@ def test_list_view_returns_dict(dummy_request):
     assert isinstance(response, dict)
 
 
-# def test_list_view_returns_len_content(dummy_request):
-#     """List view response has correct amount of content."""
+def test_list_view_returns_count_matching_database(dummy_request):
+    """List view matches database count."""
+    from pyramid_learning_journal.views.default import list_view
+    response = list_view(dummy_request)
+    query = dummy_request.dbsession.query(Entry)
+    assert len(response['entries']) == query.count()
+
+
+# def test_list_view_returns_empty_when_database_empty(dummy_request):
+#     """List view returns empty when no data in database."""
 #     from pyramid_learning_journal.views.default import list_view
 #     response = list_view(dummy_request)
-#     assert len(response['entries']) == len(ENTRIES)
+#     assert len(response['entries']) == 0
 
 
 # def test_create_view(dummy_request):
