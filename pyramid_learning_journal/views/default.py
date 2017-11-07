3dd69e6cac0047a7ae06a36e51af21904ce86db3
Allan Liebold <allan.liebold@gmail.com>
2017-11-06 16:59:59 -0800

    edit function works

--- pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/views/default.py
@@ -1,6 +1,6 @@
 """Views default.py file."""
 from pyramid.view import view_config
-from pyramid.httpexceptions import HTTPNotFound
+from pyramid.httpexceptions import HTTPFound, HTTPNotFound
 from pyramid_learning_journal.models.entries import Entry
 from datetime import datetime
 
@@ -31,24 +31,49 @@ def detail_view(request):
 @view_config(route_name='create', renderer='../templates/create.jinja2')
 def create_view(request):
     """Create view for writing a new Entry."""
-    if request.method == "POST":
-        form_data = request.POST
-        new_entry = Entry(
-            title=form_data['title'],
-            body=form_data['body'],
-            created=datetime.now(),
-        )
-        request.dbsession.add(new_entry)
-        return {}
-    return {}
+    if request.method == "POST" and request.POST:
+        form_names = ["title", "body"]
+
+        if sum([key in request.POST for key in form_names]) == len(form_names):
+
+            if '' not in list(request.POST.values()):
+                form_data = request.POST
+                new_entry = Entry(
+                    title=form_data['title'],
+                    body=form_data['body'],
+                    createreated=datetime.now(),
+                )
+            request.dbsession.add(new_entry)
+            return {}
+    data = request.POST
+    return data
 
 
 @view_config(route_name='update', renderer='../templates/update.jinja2')
 def update_view(request):
     """Update view. Edit an Entry."""
-    # target = int(request.matchdict('id'))
-    # for entry in ENTRIES:
-    #     if entry['id'] == target:
-    return {
-        # 'entry': entry
-    }
+    target = int(request.matchdict['id'])
+    entry = request.dbsession.query(Entry).get(target)
+    if request.method == "GET":
+        return {
+            'entry': entry.to_dict()
+        }
+    if request.method == "POST" and request.POST:
+        entry.body = request.POST['body']
+        entry.created = datetime.now()
+        request.dbsession.add(entry)
+        request.dbsession.flush()
+        return HTTPFound(request.route_url('detail', id=entry.id))
+    else:
+        raise HTTPNotFound
+
+
+@view_config(route_name='delete')
+def delete_view(request):
+    """Delete an entry."""
+    target = int(request.matchdict['id'])
+    entry = request.dbsession.query(Entry).get(target)
+    if entry:
+        request.dbsession.delete(Entry)
+    else:
+        raise HTTPNotFound
